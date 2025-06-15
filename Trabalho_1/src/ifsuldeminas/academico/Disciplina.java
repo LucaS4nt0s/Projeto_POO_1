0224119b0718fb51bff9a81e23d9f6055910ee47
LucaS4nt0s <lucasamuel30@gmail.com>
2025-06-15 12:19:18 -0300

    Trabalho Completo (Pode ter erro)

--- Trabalho_1/src/ifsuldeminas/academico/Disciplina.java
+++ Trabalho_1/src/ifsuldeminas/academico/Disciplina.java
@@ -40,7 +40,6 @@ public class Disciplina {
     }
 
 
-
     public String getNome() {
         return nome;
     }
@@ -135,7 +134,7 @@ public class Disciplina {
     }
 
 
-    public boolean MatricularAluno(Aluno aluno) {
+    public boolean matricularAluno(Aluno aluno) {
         if (aluno != null && !alunos.contains(aluno.getRa())) {
             alunos.add(aluno);
             notas.add(0.0);
@@ -146,7 +145,7 @@ public class Disciplina {
         }
     }
 
-    public boolean DesmatricularAluno(int posAluno){
+    public boolean desmatricularAluno(int posAluno){
         if (posAluno >= 0 && posAluno < alunos.size()) {
             alunos.remove(posAluno);
             notas.remove(posAluno);
@@ -262,4 +261,175 @@ public class Disciplina {
             System.out.println("Nenhum aluno matriculado.");
         }
     }
+
+    public int getQuantidadeReprovados() {
+        int count = 0;
+        for (int i = 0; i < alunos.size(); i++) {
+            if (!estaAprovado(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getQuantidadeAprovados() {
+        int count = 0;
+        for (int i = 0; i < alunos.size(); i++) {
+            if (estaAprovado(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void exibirProfessor() {
+        if (professor != null) {
+            System.out.println("Professor da disciplina: " + professor.getNome());
+            System.out.println("SUAP: " + professor.getSuap());
+        } else {
+            System.out.println("Disciplina não possui professor.");
+        }
+    }
+
+    public double getAproveitamentoAluno(int posAluno){
+        if (posAluno >= 0 && posAluno < alunos.size()) {
+            int frequencia = frequencias.get(posAluno);
+            double aproveitamento = (frequencia / (double) numTotalAulas) * 100;
+            return aproveitamento;
+        } else {
+            System.out.println("Posicao invalida!");
+            return -1;
+        }
+    }
+
+    public double getNotaAluno(int posAluno) {
+        if (posAluno >= 0 && posAluno < alunos.size()) {
+            return notas.get(posAluno);
+        } else {
+            System.out.println("Posicao invalida!");
+            return -1;
+        }
+    }
+
+    public void exibirAlunosAcimaMedia(){
+        if(alunos.size() == 0) {
+            System.out.println("Nenhum aluno matriculado.");
+            return;
+        }else{
+            double media = calcularMedia();
+            System.out.println("Alunos com nota acima da média (" + media + "):");
+            for (int i = 0; i < alunos.size(); i++) {
+                if (notas.get(i) > media) {
+                    System.out.println(alunos.get(i).getNome() + " - Nota: " + notas.get(i));
+                }
+            }
+            System.out.println("=================================");
+        }
+    }
+
+    public void exibirAlunosAbaixoMedia(){
+        if(alunos.size() == 0) {
+            System.out.println("Nenhum aluno matriculado.");
+            return;
+        }else{
+            double media = calcularMedia();
+            System.out.println("Alunos com nota abaixo da média (" + media + "):");
+            for (int i = 0; i < alunos.size(); i++) {
+                if (notas.get(i) < media) {
+                    System.out.println(alunos.get(i).getNome() + " - Nota: " + notas.get(i));
+                }
+            }
+            System.out.println("=================================");
+        }
+    }
+
+    public void exibirAlunoNotaAproveitamento(){
+        if(alunos.size() == 0) {
+            System.out.println("Nenhum aluno matriculado.");
+            return;
+        }else{
+            System.out.println("Alunos, notas e aproveitamento:");
+            for (int i = 0; i < alunos.size(); i++) {
+                double aproveitamento = getAproveitamentoAluno(i);
+                double nota = getNotaAluno(i);
+                System.out.println("Aluno: " + alunos.get(i).getNome() + ", Nota: " + nota + ", Aproveitamento: " + aproveitamento + "%");
+            }
+            System.out.println("=================================");
+        }
+    }
+
+    public void desmatricularAluno(){
+        if (alunos.size() == 0) {
+            System.out.println("Nenhum aluno matriculado.");
+            return;
+        }
+        for (Aluno aluno : alunos) {
+            System.out.println("Desmatriculando aluno: " + aluno.getNome());
+            int posAluno = alunos.indexOf(aluno);
+            notas.remove(posAluno);
+            frequencias.remove(posAluno);
+            aluno.remove(this);
+            System.out.println("Aluno " + aluno.getNome() + " desmatriculado com sucesso.");
+            System.out.println("=================================");
+        }
+    }
+
+    public void exibirOrdenadosPorNota(){
+        if (alunos.size() == 0) {
+            System.out.println("Nenhum aluno matriculado.");
+            return;
+        }
+
+        ArrayList<Aluno> alunosOrdenados = new ArrayList<>(alunos);
+        ArrayList<Double> notasOrdenadas = new ArrayList<>(notas);
+
+        // Ordena as notas e os alunos correspondentes por nota (maior para menor)
+        for (int i = 0; i < notasOrdenadas.size() - 1; i++) {
+            for (int j = i + 1; j < notasOrdenadas.size(); j++) {
+                if (notasOrdenadas.get(i) > notasOrdenadas.get(j)) {
+                    // Troca as notas
+                    double tempNota = notasOrdenadas.get(i);
+                    notasOrdenadas.set(i, notasOrdenadas.get(j));
+                    notasOrdenadas.set(j, tempNota);
+
+                    // Troca os alunos correspondentes
+                    Aluno tempAluno = alunosOrdenados.get(i);
+                    alunosOrdenados.set(i, alunosOrdenados.get(j));
+                    alunosOrdenados.set(j, tempAluno);
+                }
+            }
+        }
+
+        System.out.println("Alunos ordenados por nota (maior para menor):");
+        for (int i = 0; i < alunosOrdenados.size(); i++) {
+            System.out.println(alunosOrdenados.get(i).getNome() + " - Nota: " + notasOrdenadas.get(i));
+        }
+        System.out.println("=================================");
+    }
+
+    public void exibirDisciplina() {
+        if (alunos.size() == 0) {
+            System.out.println("Nenhum aluno matriculado.");
+            return;
+        }
+        System.out.println("Nome: " + getNome());
+        System.out.println("Periodo: " + getPeriodo());
+        System.out.println("Numero de aulas por semana: " + getNumAulasSemana());
+        System.out.println("Numero total de aulas: " + getNumTotalAulas());
+        exibirProfessor();
+        System.out.println("Numero de alunos matriculados: " + getNumAlunos());
+        System.out.println("Alunos matriculados:");
+        for (Aluno aluno : alunos) {
+            System.out.println(" - " + aluno.getNome() + " (RA: " + aluno.getRa() + ")");
+            posAluno = alunos.indexOf(aluno);
+            System.out.println("   Nota: " + notas.get(posAluno));
+            System.out.println("   Frequencia: " + frequencias.get(posAluno) + "/" + numTotalAulas);
+        }
+        System.out.println("=================================");
+        System.out.println("Media da disciplina: " + calcularMedia());
+        System.out.println("Quantidade de alunos" + getNumAlunos());
+        System.out.println("Quantidade de alunos aprovados: " + getQuantidadeAprovados());
+        System.out.println("Quantidade de alunos reprovados: " + getQuantidadeReprovados());
+    }
+
 }
---
+++ Trabalho_1/src/ifsuldeminas/main/Main.java
@@ -0,0 +1,173 @@
+package ifsuldeminas.main;
+import java.util.Scanner;
+import ifsuldeminas.alunos.Aluno;
+import ifsuldeminas.disciplinas.Disciplina;
+import ifsuldeminas.funcionarios.Professor;
+
+public class Main{
+    public static void main(String[] args) {
+        Disciplina disciplina = new Disciplina("Programação Orientada a Objetos", 1, 4, 15);
+
+        Aluno aluno1 = new Aluno("João Silva", 1001, 1, "Engenharia da Computação");
+        Aluno aluno2 = new Aluno("Maria Oliveira", 1002, 1, "Engenharia da Computação");
+        Aluno aluno3 = new Aluno("Pedro Santos", 1003, 1, "Engenharia da Computação");
+        Aluno aluno4 = new Aluno("Ana Costa", 1004, 1, "Engenharia da Computação");
+        Aluno aluno5 = new Aluno("Lucas Pereira", 1005, 1, "Engenharia da Computação");
+        Aluno aluno6 = new Aluno("Fernanda Lima", 1006, 1, "Engenharia da Computação");
+        Aluno aluno7 = new Aluno("Carla Souza", 1007, 1, "Engenharia da Computação");
+        Aluno aluno8 = new Aluno("Rafael Almeida", 1008, 1, "Engenharia da Computação");
+        Aluno aluno9 = new Aluno("Juliana Rocha", 1009, 1, "Engenharia da Computação");
+        Aluno aluno10 = new Aluno("Gabriel Martins", 1010, 1, "Engenharia da Computação");
+
+        disciplina.matricularAluno(aluno1);
+        disciplina.matricularAluno(aluno2);
+        disciplina.matricularAluno(aluno3);
+        disciplina.matricularAluno(aluno4);
+        disciplina.matricularAluno(aluno5);
+        disciplina.matricularAluno(aluno6);
+        disciplina.matricularAluno(aluno7);
+        disciplina.matricularAluno(aluno8);
+        disciplina.matricularAluno(aluno9);
+        disciplina.matricularAluno(aluno10);
+
+        disciplina.addNota(0, 8.5);
+        disciplina.addNota(1, 7.0);
+        disciplina.addNota(2, 4.0);
+        disciplina.addNota(3, 6.5);
+        disciplina.addNota(4, 10.0);
+        disciplina.addNota(5, 5.0);
+        disciplina.addNota(6, 8.0);
+        disciplina.addNota(7, 5.5);
+        disciplina.addNota(8, 6.0);
+        disciplina.addNota(9, 9.5);
+
+        disciplina.addFrequencia(0, 60);
+        disciplina.addFrequencia(1, 56);
+        disciplina.addFrequencia(2, 40);
+        disciplina.addFrequencia(3, 50);
+        disciplina.addFrequencia(4, 30);
+        disciplina.addFrequencia(5, 22);
+        disciplina.addFrequencia(6, 38);
+        disciplina.addFrequencia(7, 60);
+        disciplina.addFrequencia(8, 58);
+        disciplina.addFrequencia(9, 28);
+
+        Professor professor = new Professor("Carlos Alberto", 1234);
+        disciplina.setProfessor(professor);
+
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do{
+            System.out.println("Menu:");
+            System.out.println("Digite o número da opção desejada:");
+            System.out.println("0 - Sair (encerrar o programa)");
+            System.out.println("1 - Imprimir informações da disciplina");
+            System.out.println("2 - Apresentar número de alunos na disciplina, número de reprovados e número de aprovados;");
+            System.out.println("3 - Imprimir aprovados");
+            System.out.println("4 - Imprimir reprovados");
+            System.out.println("5 - Imprimir aluno com a maior e o aluno com menor nota");
+            System.out.println("6 - Imprimir o professor responsável");
+            System.out.println("7 - Imprimir média total da turma e listar todos os alunos acima da média");
+            System.out.println("8 - Imprimir média total da turma e listar todos os alunos abaixo da média");
+            System.out.println("9 - Imprimir nome dos alunos e suas respectivas notas e aproveitamento");
+            System.out.println("10 - Imprimir alunos ordenados por nota");
+            System.out.println("11 - Matricular aluno");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 0:
+                    System.out.println("Encerrando o programa...");
+                    disciplina.desmatricularAluno();
+                    disciplina.removerProfessor();
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                case 1:
+                    System.out.println("Informações da Disciplina:");
+                    disciplina.exibirDisciplina();
+                    break;
+                case 2:
+                    System.out.println("Número de Alunos: " + disciplina.getNumAlunos());
+                    System.out.println("Número de Reprovados: " + disciplina.getNumReprovados());
+                    System.out.println("Número de Aprovados: " + disciplina.getNumAprovados());
+                    break;
+                case 3:
+                    System.out.println("Aprovados:");
+                    disciplina.exibirAprovados();
+                    break;
+                case 4:
+                    System.out.println("Reprovados:");
+                    disciplina.exibirReprovados();
+                    break;
+                case 5:
+                    System.out.println("Aluno com a Maior Nota:");
+                    disciplina.exibirMaiorNota();
+                    System.out.println("==========================");
+                    System.out.println("Aluno com a Menor Nota:");
+                    disciplina.exibirMenorNota();
+                    break;
+                case 6:
+                    System.out.println("Professor Responsável:");
+                    disciplina.exibirProfessor();
+                    break;
+                case 7:
+                    System.out.println("Média Total da Turma: " + disciplina.calcularMedia());
+                    System.out.println("Alunos Acima da Média:");
+                    disciplina.exibirAlunosAcimaMedia();
+                    break;
+                case 8:
+                    System.out.println("Média Total da Turma: " + disciplina.calcularMedia());
+                    System.out.println("Alunos Abaixo da Média:");
+                    disciplina.exibirAlunosAbaixoMedia();
+                    break;
+                case 9:
+                    System.out.println("Nome dos Alunos e suas Respectivas Notas e Aproveitamento:");
+                    disciplina.exibirAlunoNotaAproveitamento();
+                    break;
+                case 10:
+                    System.out.println("Alunos Ordenados por Nota:");
+                    disciplina.exibirOrdenadosPorNota();
+                    break;
+                case 11:
+                    do{
+                        System.out.println("Matricular Aluno:");
+                        System.out.print("Digite o nome do aluno: ");
+                        scanner.nextLine(); 
+                        String nome = scanner.nextLine();
+                        System.out.print("Digite o curso do aluno: ");
+                        String curso = scanner.nextLine();
+                        System.out.print("Digite o RA do aluno: ");
+                        int ra = scanner.nextInt();
+                        Aluno novoAluno = new Aluno(nome, ra, 1, curso);
+                    }while(!novoAluno.isValid());
+
+                    boolean matriculaValida = disciplina.matricularAluno(novoAluno);
+                    if(matriculaValida) {
+                        System.out.println("Aluno " + novoAluno.getNome() + " matriculado com sucesso!");
+                    } else {
+                        System.out.println("Não foi possível matricular o aluno. RA semelhante já cadastrado");
+                        return;
+                    }
+                    
+                    do{
+                        boolean notaValida = false;
+                        System.out.println("Digite a nota do aluno: ");
+                        double nota = scanner.nextDouble();
+                        notaValida = disciplina.addNota(disciplina.getNumAlunos() - 1, nota);
+                    }while(!notaValida);
+
+                    do{
+                        boolean frequenciaValida = false;
+                        System.out.println("Digite a frequência do aluno: ");
+                        int frequencia = scanner.nextInt();
+                        frequenciaValida = disciplina.addFrequencia(disciplina.getNumAlunos() - 1, frequencia);
+                    }while(!frequenciaValida);
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        }
+    }
+}
